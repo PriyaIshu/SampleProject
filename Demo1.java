966682364021c9553fe1d9742385bd3041905f47
PriyaIshu <ishupriya130@gmail.com>
2019-03-22 21:33:40 +0530

    Added demo and java files

---
+++ Demo1.java
@@ -0,0 +1,44 @@
+class Demo1
+{
+static int a=1000;
+int b=2000;
+Demo1()
+{
+}
+Demo1(int a)
+{
+this(10,20);
+System.out.println("1 Argumented Constructor");
+int b=1000;
+System.out.println(a+b);
+}
+Demo1(int a,int b)
+{
+System.out.println("2 Argumented Constructor");
+}
+static int add(int a,int b)
+{
+System.out.println("Static Method");
+return a+b;
+}
+void add1(int a,int b)
+{
+System.out.println("Non-Static Method");
+int sum=this.a+b;
+System.out.println("Sum="+sum);
+}
+public static void main(String args[])
+{
+int a=10;
+int  b=100;
+System.out.println(a);
+System.out.println(b);
+Demo1 d=new Demo1();
+Demo1 d1=new Demo1(10);
+System.out.println(Demo1.a);
+System.out.println(d.b);
+int x=Demo1.add(10,10);
+System.out.println("Add value="+x);
+d1.add1(100,200);
+}
+}
---
+++ Even.java
@@ -0,0 +1,15 @@
+class Even
+{
+public static void main(String args[])
+{
+int number=5;
+int fact=1;
+int i=1;
+while(i<=number)
+{
+fact*=i;
+i++;
+}
+System.out.println(fact);
+}
+}
